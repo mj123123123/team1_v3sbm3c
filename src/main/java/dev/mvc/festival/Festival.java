e894ef0b35c5aea347023b7ebd851b8fc2dc92e0
hyeonsu6 <bhshs0508@naver.com>
2023-12-08 15:05:12 +0900

    festival 전체 수정

--- src/main/java/dev/mvc/festival/Festival.java
+++ src/main/java/dev/mvc/festival/Festival.java
@@ -16,12 +16,12 @@ public class Festival {
         String path = "";
         if (File.separator.equals("\\")) { // windows, 개발 환경의 파일 업로드 폴더
             // path = "C:/kd/deploy/team1_v4sbm3c/contents/storage/";
-            path="C:\\kd\\deploy\\team1_v4sbm3c\\contents\\storage\\";
+            path="C:\\kd\\deploy\\team1_v3sbm3c\\festival\\storage\\";
             // System.out.println("Windows 10: " + path);
             
         } else { // Linux, AWS, 서비스용 배치 폴더 
             // System.out.println("Linux");
-            path = "/home/ubuntu/deploy/team1_v4sbm3c/contents/storage/";
+            path = "/home/ubuntu/deploy/team1_v3sbm3c/festival/storage/";
         }
         
         return path;
