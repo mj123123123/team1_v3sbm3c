38b1ea61b099885bc094745aef96971422340f78
hyeonsu6 <bhshs0508@naver.com>
2023-12-12 12:09:21 +0900

    리뷰 sql 수정

--- src/main/webapp/WEB-INF/doc/festival/review.sql
+++ src/main/webapp/WEB-INF/doc/festival/review.sql
@@ -1,20 +1,21 @@
 /**********************************/
 /* Table Name: 축제/행사 리뷰 */
 /**********************************/
-CREATE TABLE REVIEW(
+CREATE TABLE FREVIEW(
 		reviewno NUMBER(10) NOT NULL PRIMARY KEY,
-		fcontentsno NUMBER(10),
+		contentsno NUMBER(10),
 		MEMBERNO NUMERIC(10),
 		title VARCHAR2(100) NOT NULL,
 		content CLOB NOT NULL,
 		cnt NUMBER(10),
+        pwd VARCHAR2(10),
 		rdate DATE NOT NULL,
-		reviewfile1 VARCHAR2(200),
-		reviewfile1saved VARCHAR2(400),
-		reviewthumb1 VARCHAR2(200),
-		reviewsize1 NUMBER(10),
-		reviewmap INTEGER(10),
-  FOREIGN KEY (fcontentsno) REFERENCES FESTIVAL (fcontentsno),
+		file1 VARCHAR2(200),
+        file1saved VARCHAR2(400),
+		thumb1 VARCHAR2(200),
+		size1 NUMBER(10),
+		map VARCHAR2(2000),
+  FOREIGN KEY (contentsno) REFERENCES FESTIVAL (contentsno),
   FOREIGN KEY (MEMBERNO) REFERENCES MEMBER (MEMBERNO)
 );
 
@@ -26,3 +27,9 @@ CREATE SEQUENCE review_seq
   MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
   CACHE 2                        -- 2번은 메모리에서만 계산
   NOCYCLE;                      -- 다시 1부터 생성되는 것을 방지
+  
+  
+  
+  
+  
+  
